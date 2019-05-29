287173425d8190395a676d4a069bbc3be6a2a128
hoon <donghun4754@gmail.com>
2019-05-29 11:53:27 +0900

    Add existing file

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+//import { Server } from 'http';
+
+@Component({
+    selector : 'app-server',
+    templateUrl : './server.component.html',
+    styleUrls: ['./server.component.css']
+})
+export class ServerComponent {
+
+}
